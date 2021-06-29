02c665d1ee0cf0c401a51d24e221dcefc2bed4b6
EajayD <edelos1524@hotmail.com>
2021-06-28 18:26:29 -0700

    added questions, added function start(onclick) to HTML, tested function for first question

--- script.js
+++ script.js
@@ -10,3 +10,53 @@ var choiceFour = document.querySelector("#select4");
 
 // Hides question area on start up
 questions.style.display = "none";
+
+var inquiry = [
+    { 
+        question: "What does CSS stand for?",
+        c1: "Carrying Slow Snakes",
+        c2: "Cascading Style Sheet",
+        c3: "Calling Steven Spielberg",
+        c4: "Caressing Soft Snowmen"
+    },
+    {
+        question: " A boolean is ______.",
+        c1: "a ghost",
+        c2: "a string of elements",
+        c3: "an array",
+        c4: "a data type"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables",
+        c1: "quotation marks",
+        c2: "a blanket",
+        c3: "curly braces",
+        c4: "parenthesis"
+    },
+    {
+        question: "In CSS, when something is written as 'button:hover' for example; hover is considered what?",
+        c1: "A concat variable",
+        c2: "A psuedo class",
+        c3: "A function",
+        c4: "A reclassification"
+    },
+    {
+        question: "What does DOM stand for?",
+        c1: "Deliberate Order Management",
+        c2: "Designing Other Messages",
+        c3: "Document Object Model",
+        c4: "Don't Open Master"
+    }
+];
+
+function beginTest() {
+    questions.style.display = "block";
+    instructions.style.display = "none";
+    start.style.display = "none";
+
+    ask.textContent = (inquiry[0].question);
+    choiceOne.textContent = (inquiry[0].c1);
+    choiceTwo.textContent = (inquiry[0].c2);
+    choiceThree.textContent = (inquiry[0].c3);
+    choiceFour.textContent = (inquiry[0].c4);
+}
