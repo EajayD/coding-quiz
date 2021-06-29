4670f2b1b053facbaa896fdabcbafd272cc8bd79
EajayD <edelos1524@hotmail.com>
2021-06-28 17:42:44 -0700

    added variable pointers to js, and hid the questions area

---
+++ script.js
@@ -0,0 +1,12 @@
+// Pointers
+var instructions = document.querySelector(".instructions");
+var questions = document.querySelector(".questions");
+var start = document.querySelector("#start");
+var ask = document.querySelector("#ask");
+var choiceOne = document.querySelector("#select1");
+var choiceTwo = document.querySelector("#select2");
+var choiceThree = document.querySelector("#select3");
+var choiceFour = document.querySelector("#select4");
+
+// Hides question area on start up
+questions.style.display = "none";
